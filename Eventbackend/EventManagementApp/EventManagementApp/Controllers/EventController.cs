143a6a3a0f456f7fbda5577fbc1cc218db1a0c84
Ayesha Shaik <2000112369@hexaware.com>
2024-02-15 18:07:50 +0530

    Init

---
+++ Eventbackend/EventManagementApp/EventManagementApp/Controllers/EventController.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using EventManagementApp.Exceptions;
+using EventManagementApp.Interfaces;
+using EventManagementApp.Models;
+
+namespace EventManagementApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventController : ControllerBase
+    {
+        private readonly IEventService _eventService;
+
+        public EventController(IEventService bookService)
+        {
+            _eventService = bookService;
+        }
+        [HttpGet]
+        public ActionResult GetAllEvents()
+        {
+            string errorMessage = string.Empty;
+            try
+            {
+                var result = _eventService.GetEvents();
+                return Ok(result);
+            }
+            catch (NoEventsAvailableException e)
+            {
+                errorMessage = e.Message;
+            }
+            return BadRequest(errorMessage);
+        }
+        [HttpGet("GetByUserId")]
+        public ActionResult GetById(string id)
+        {
+            string message = string.Empty;
+            try
+            {
+                var result = _eventService.GetByUserId(id);
+                if (result != null)
+                {
+                    return Ok(result);
+                }
+                message = "Could not Get book";
+            }
+            catch (Exception e)
+            {
+                message = e.Message;
+            }
+            return BadRequest(message);
+        }
+        [HttpGet("GetByEventId")]
+        public ActionResult GetEventId(int id)
+        {
+            string message = string.Empty;
+            try
+            {
+                var result = _eventService.GetByEventId(id);
+                if (result != null)
+                {
+                    return Ok(result);
+                }
+                message = "Could not Get book";
+            }
+            catch (Exception e)
+            {
+                message = e.Message;
+            }
+            return BadRequest(message);
+        }
+        [HttpPost("AddEvent")]
+        //[Authorize(Roles = "Admin")]
+        public ActionResult Create(Event events)
+        {
+            string errorMessage = string.Empty;
+            try
+            {
+                var result = _eventService.Add(events);
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+                errorMessage = e.Message;
+            }
+            return BadRequest(errorMessage);
+        }
+        [HttpPost]
+        //[Authorize(Roles = "Admin")]
+        [Route("RemoveEvent")]
+        public ActionResult RemoveEvent(int Id)
+        {
+            string errorMessage = string.Empty;
+            try
+            {
+                var result = _eventService.Remove(Id);
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+                errorMessage = e.Message;
+            }
+            return BadRequest(errorMessage);
+        }
+        [HttpPut]
+        public ActionResult Update(Event events)
+        {
+            string errorMessage = string.Empty;
+            try
+            {
+                var result = _eventService.Update(events);
+                return Ok(events);
+            }
+            catch (EventsCantUpdateException e)
+            {
+                errorMessage = e.Message;
+            }
+            return BadRequest(errorMessage);
+        }
+
+        [HttpDelete]
+        public ActionResult Remove(int Id)
+        {
+            string errorMessage = string.Empty;
+            try
+            {
+                var result = _eventService.Remove(Id);
+                return Ok("event deleted successfully");
+            }
+            catch (EventsCantRemoveException e)
+            {
+                errorMessage = e.Message;
+            }
+            return BadRequest(errorMessage);
+        }
+    }
+}
---
+++ Eventbackend/EventManagementApp/EventManagementApp/Services/EventService.cs
@@ -0,0 +1,92 @@
+﻿using EventManagementApp.Interfaces;
+using EventManagementApp.Models;
+using EventManagementApp.Exceptions;
+using EventManagementApp.Repositories;
+using static System.Reflection.Metadata.BlobBuilder;
+
+namespace EventManagementApp.Services
+{
+    public class EventService : IEventService
+    {
+        private readonly IEventService _eventRepository;
+        private readonly IRepository<int, Event> repository;
+        public EventService(IRepository<int, Event> _repository)
+        {
+            repository = _repository;
+        }
+
+        public Event Add(Event events)
+        {
+            var res = repository.Add(events);
+            return res;
+        }
+
+        public Event Update(Event events)
+        {
+            var EventTitle = repository.GetAll().FirstOrDefault(e => e.Id == events.Id);
+            if (EventTitle != null)
+            {
+                var result = repository.Update(events);
+                if (result != null) return result;
+            }
+            return EventTitle;
+        }
+        public Event Remove(int Id)
+        {
+            var EventId = repository.GetAll().FirstOrDefault(e => e.Id == Id);
+            if (EventId != null)
+            {
+                var result = repository.Delete(EventId.Id);
+                if (result != null) return result;
+            }
+            return EventId;
+        }
+
+
+        public List<Event> GetEvents()
+        {
+            var events = repository.GetAll();
+            if (events != null)
+            {
+                return events.ToList();
+            }
+            throw new NoEventsAvailableException();
+        }
+
+        public List<Event> GetByUserId(string UserId)
+        {
+            try
+            {
+                var events = repository.GetAll().Where(c => c.UserId == UserId).ToList();
+                if (events != null)
+                {
+                    return events;
+                }
+            }
+            catch (Exception)
+            {
+                throw new NoEventsAvailableException();
+            }
+            return null;
+        }
+        public List<Event> GetByEventId(int Id)
+        {
+            try
+            {
+                var events = repository.GetAll().Where(e => e.Id == Id).ToList();
+                if (events != null)
+                {
+                    return events;
+                }
+            }
+            catch (Exception)
+            {
+                throw new NoEventsAvailableException();
+            }
+            return null;
+        }
+
+
+
+    }
+}
